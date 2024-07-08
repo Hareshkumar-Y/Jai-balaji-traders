fb12ee045797f2bd57a899eec4d5a07993c8f85a
Hareshkumar-Y <68243286+Hareshkumar-Y@users.noreply.github.com>
2024-07-08 20:25:01 +0530

    ...

--- website_files/scripts.js
+++ website_files/scripts.js
@@ -7,18 +7,28 @@ menu.addEventListener('click',()=>{
 })
 
 window.onscroll = () => {
+    console.log('Scrolling...');
     sections.forEach((sec) => {
         let top = window.scrollY;
         let offset = window.scrollY;
         let height = sec.offsetHeight - 150;
         let id = sec.getAttribute('id')
 
+        console.log(`Section ${id} offsetTop: ${sec.offsetTop}, height: ${height}`);
+
         if (top >= sec.offsetTop && top < (sec.offsetTop + height)) {
+            console.log(`Section ${id} is in viewport`);
             let navLinks = navUL.querySelectorAll('li') // Get all li elements inside navUL
             navLinks.forEach((link) => {
                 link.classList.remove('active');
-            document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+                console.log(`Removing active class from link ${link.textContent}`);
+                let navLink = document.querySelector('nav a[href*=' + id + ']');
+                console.log(`Found nav link with href*=${id}: ${navLink}`);
+                navLink.classList.add('active');
+                console.log(`Added active class to nav link ${navLink.textContent}`);
             })
+        } else {
+            console.log(`Section ${id} is not in viewport`);
         }
     });
 };
